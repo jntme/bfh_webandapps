13cc868039112f01be4514884da137d7ab5fdae2
jntme <mail@jntme.ch>
2017-02-24 11:45:30 +0100

    add first files of first exercise

---
+++ exercises/mytests/arrays.php
@@ -0,0 +1,21 @@
+
+<?php
+
+$arrayD = array();
+
+
+$arrayD[] = 'Hello';
+$arrayD[] = 'World';
+
+$arrayD[5] = 'Five';
+
+
+//print_r($arrayD);
+
+foreach($arrayD as $i) {
+    echo $i;
+}
+
+echo isset($arrayD);
+
+?>
