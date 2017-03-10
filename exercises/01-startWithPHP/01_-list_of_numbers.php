ed5ffa5f664f9599b0088625bb8abaf58407e16d
jntme <mail@jntme.ch>
2017-03-10 10:23:44 +0100

    renaming files and added 01

---
+++ exercises/01-startWithPHP/01_-list_of_numbers.php
@@ -0,0 +1,38 @@
+<?php
+
+if (isset($_GET['textfield']) and ($_GET['textfield'] != "")) {
+    $number = $_GET['textfield'];
+
+    if (is_numeric($number)) {
+
+        for($i = 1; $i < $number; $i++) {
+            echo "- $i";
+        }
+
+    }
+    else {
+        echo "hello";
+    }
+
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>list of numbers</title>
+</head>
+<body>
+<form action="#" method="get">
+    <input type="text" name="textfield"/>
+    <button action="submit">go!</button>
+</form>
+</body>
+</html>
+
+
