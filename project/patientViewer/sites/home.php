58cc80ddc12c8ffa6d46310ae8542f8e96ef9f39
FCald <fabienne_calderara@hotmail.com>
2017-05-05 10:42:15 +0200

    Anpassungen home, overview
    overview: Bild für Medikation zu Leaf geändert
    home: Text hinzugefügt

--- project/patientViewer/sites/home.php
+++ project/patientViewer/sites/home.php
@@ -1,4 +1,10 @@
 <div class="starter-template">
 <h1>Welcome to the PatientViewer!</h1>
-<p class="lead">Try to login yourself.</p>
+<p class="lead">Try to login yourself.</p><br>
+<p>Mit dieser App können Sie die Vitalwerte und die Medikation Ihrer Patienten verwalten.</p><br>
+
+<h2 >Features</h2><br>
+<p>Patienten hinzufügen</p>
+<p>Vitalwerte hinzufügen und abrufen</p>
+<p>Abgegebene Medikamente hinzufügen und abrufen</p>
 </div>
